0d1275e3215c8c0799db37f321cfb60df4c2d5fa
Laurence Gonsalves <laurence@xenomachina.com>
2014-07-10 11:48:59 -0700

    Add StatusIcon when mount added.

--- gvfs_tray.py
+++ gvfs_tray.py
@@ -16,6 +16,7 @@ from gi.repository import GObject
 from gi.repository import Gtk
 from gi.repository import Notify
 
+from cgi import escape as htmlEscape
 from pprint import pprint
 
 __author__  = 'Laurence Gonsalves <laurence@xenomachina.com>'
@@ -32,42 +33,31 @@ def dump_event(event, mount):
     mount_path = mount.get_root().get_path()
     print("%s %s" % (mount_path, event))
 
-def on_mount_added(volume_monitor, mount, *user_args):
-    dump_event("added", mount)
+class IconManager:
+    def __init__(self):
+        self.icons = {}
 
-def on_mount_changed(volume_monitor, mount, *user_args):
-    dump_event("changed", mount)
+    def on_mount_added(self, volume_monitor, mount, *user_args):
+        dump_event("added", mount)
+        self.create_icon(mount)
 
-def on_mount_pre_unmount(volume_monitor, mount, *user_args):
-    dump_event("pre-unmount", mount)
+    def on_mount_changed(self, volume_monitor, mount, *user_args):
+        dump_event("changed", mount)
 
-def on_mount_removed(volume_monitor, mount, *user_args):
-    dump_event("removed", mount)
+    def on_mount_pre_unmount(self, volume_monitor, mount, *user_args):
+        dump_event("pre-unmount", mount)
 
-def on_volume_mounted(volume, async_result, mount_op):
-    if volume.mount_finish(async_result):
-        print("MOUNTED VOLUME")
-        print_volume_identifiers(volume)
+    def on_mount_removed(self, volume_monitor, mount, *user_args):
+        dump_event("removed", mount)
 
-        label = volume.get_identifier('label')
-        label = "Unlabeled volume" if label is None else repr(label)
-        mount_path = volume.get_mount().get_root().get_path()
-
-        notification = Notify.Notification.new(
-                "Volume mounted",
-                "%s mounted at %s" % (label, mount_path),
-                "dialog-information")
-        #notification.add_action("action_open", "Open", dump_to_stdout)
-        notification.connect("closed", on_notification_closed, None)
-
-        # TODO: get icon from volume?
-        # TODO: is it possible to respond to clicks on notification?
-        notification.show()
-
-def create_icon(mount):
-    icon = Gtk.StatusIcon.new_from_icon_name('drive-removable-media')
-    icon.set_visible(True)
-    icon.set_name("Foo")
+    def create_icon(self, mount):
+        label = mount.get_name()
+        path = mount.get_root().get_path()
+        icon = Gtk.StatusIcon.new_from_gicon(mount.get_icon())
+        icon.set_tooltip_markup("%s <tt>%s</tt>"
+                % tuple(map(htmlEscape, (label, path))))
+        icon.set_visible(True)
+        self.icons[path] = icon
 
 class UserError(Exception):
     def __init__(self, message):
@@ -80,16 +70,17 @@ def create_parser():
     return parser
 
 def main(args):
+    tray = IconManager()
     vm = Gio.VolumeMonitor.get()
     connections = []
-    connections.append(vm.connect("mount-added", on_mount_added, 1))
-    connections.append(vm.connect("mount-changed", on_mount_changed, 1))
-    connections.append(vm.connect("mount-pre-unmount", on_mount_pre_unmount, 1))
-    connections.append(vm.connect("mount-removed", on_mount_removed, 1))
+    connections.append(vm.connect("mount-added", tray.on_mount_added, 1))
+    connections.append(vm.connect("mount-changed", tray.on_mount_changed, 1))
+    connections.append(vm.connect("mount-pre-unmount", tray.on_mount_pre_unmount, 1))
+    connections.append(vm.connect("mount-removed", tray.on_mount_removed, 1))
 
     # Synthesize mount-added events for pre-existing mounts
     for mount in vm.get_mounts():
-        on_mount_added(vm, mount, 0)
+        tray.on_mount_added(vm, mount, 0)
 
     GObject.MainLoop().run()
 
